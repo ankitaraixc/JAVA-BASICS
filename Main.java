40b5925295bafb7051b1c2ae3de604d7b1fb0519
ankitaraixc <ankitaraixc@gmail.com>
2025-07-10 08:11:25 +0530

    main

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        // System.out.println("hello world");
+       // System.out.println("hey how are you");
+        Scanner input = new Scanner(System.in);
+        // System.out.println(input.nextInt()); // for input the next integer
+        System.out.println(input.nextLine());//for input the next line
+    }
+}

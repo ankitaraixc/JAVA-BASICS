e8bf10d6edef181b7ddd2db5713732c535aec5ff
ankitaraixc <ankitaraixc@gmail.com>
2025-07-10 08:11:41 +0530

    primitives

---
+++ Primitive.java
@@ -0,0 +1,11 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int rollno = 64;
+        String name ="sneha"; // String is not primitive
+        char letter='g';
+        float marks = 78.3f;
+        double largeDecimalNumbers=13124435.4556;
+        long largeInteger=232335267L;
+        boolean check = true;
+    }
+}
